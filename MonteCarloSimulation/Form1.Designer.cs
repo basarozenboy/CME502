7e168d669008356c53b2652126d4634a8090d08a
basarozenboy <pashaoo@gmail.com>
2015-03-16 23:11:18 +0200

    Hw1

---
+++ MonteCarloSimulation/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace MonteCarloSimulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button3 = new System.Windows.Forms.Button();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Test Function";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(91, 224);
+            this.chart1.Name = "chart1";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series5.Legend = "Legend1";
+            series5.MarkerSize = 3;
+            series5.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series5.Name = "test1";
+            series5.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series5.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(439, 200);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 49);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Test Random Numb";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // chart2
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart2.Legends.Add(legend5);
+            this.chart2.Location = new System.Drawing.Point(93, 12);
+            this.chart2.Name = "chart2";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series6.Legend = "Legend1";
+            series6.MarkerSize = 3;
+            series6.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series6.Name = "test1";
+            series6.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series6.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            this.chart2.Series.Add(series6);
+            this.chart2.Size = new System.Drawing.Size(439, 200);
+            this.chart2.TabIndex = 4;
+            this.chart2.Text = "chart2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 236);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Numb. of Trial";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(10, 361);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(75, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 302);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Trial Result";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 345);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Real Result";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 385);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Difference";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(10, 401);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(75, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // chart3
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart3.Legends.Add(legend6);
+            this.chart3.Location = new System.Drawing.Point(93, 430);
+            this.chart3.Name = "chart3";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series7.Legend = "Legend1";
+            series7.MarkerSize = 3;
+            series7.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series7.Name = "test1";
+            series7.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series7.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series8.Legend = "Legend1";
+            series8.Name = "test2";
+            this.chart3.Series.Add(series7);
+            this.chart3.Series.Add(series8);
+            this.chart3.Size = new System.Drawing.Size(439, 200);
+            this.chart3.TabIndex = 13;
+            this.chart3.Text = "chart3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 443);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Start of Trial";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 461);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(75, 20);
+            this.textBox5.TabIndex = 14;
+            this.textBox5.Text = "1000";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 484);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Inc. of Trail";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(10, 504);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(75, 20);
+            this.textBox6.TabIndex = 16;
+            this.textBox6.Text = "1000";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(10, 576);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 34);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Test Function";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 530);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Inc. Time";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(10, 550);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(75, 20);
+            this.textBox7.TabIndex = 19;
+            this.textBox7.Text = "10";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 318);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 640);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Monte Carlo Simuation";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ MonteCarloSimulation/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//using System.Windows.Forms.DataVisualization.Charting.Chart;
+
+namespace MonteCarloSimulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static readonly Random random = new Random();
+        public static double GetRandomNumber(double minimum, double maximum)
+        {
+            { 
+                return random.NextDouble() * (maximum - minimum) + minimum;
+            }
+        }
+
+        private void testRandomNumberGenerator(int numbOf)
+        {
+            double testNumb;
+            string lines = "";
+            clearChart(chart2);
+            for (int i = 1; i <= numbOf; i++)
+            {
+                testNumb = GetRandomNumber(0, 1);
+                chart2.Series["test1"].Points.AddXY(i, testNumb);
+                lines = lines + testNumb.ToString() + "\r\n";
+            }
+            // Write the string to a file.
+            System.IO.StreamWriter file = new System.IO.StreamWriter("c:\\testNumb.txt");
+            file.WriteLine(lines);
+            file.Close();
+        }
+
+        private void clearChart( System.Windows.Forms.DataVisualization.Charting.Chart chartx)
+        {
+            foreach (var series in chartx.Series)
+                series.Points.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            testMonteCarloSimulation();
+        }
+
+        private void testMonteCarloSimulation()
+        {
+            double x, y, Area;
+            int K = 0;
+            int N = Int32.Parse(textBox1.Text);
+            string lines = "";
+            clearChart(chart1);
+            for (int i = 0; i < N; i++)
+            {
+                x = GetRandomNumber(1, 4);
+                y = GetRandomNumber(0, 2);
+                if ((y <= (Math.Sqrt(x) )) && (x<= 4) && (x>=1) && (y>=0) && (y<=2))
+	            {
+                    K++;
+                    lines = lines + x.ToString() + ' ' + y.ToString() + "\r\n";
+                    chart1.Series["test1"].Points.AddXY(x, y);
+	            }              
+            }
+            Area = ((double)K /(double)N) * 2.0 * 3.0;
+            textBox2.Text = Area.ToString();
+            textBox3.Text = (14.0/3.0).ToString();
+            textBox4.Text = Math.Abs((Area - (14.0 / 3.0))).ToString();
+            // Write the string to a file.
+            System.IO.StreamWriter file = new System.IO.StreamWriter("c:\\test.txt");
+            file.WriteLine(lines);
+
+            file.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {
+                Random rdn = new Random();
+                for (int i = 0; i < 50; i++)
+                {
+                    chart1.Series["test1"].Points.AddXY
+                                    (rdn.NextDouble(), rdn.NextDouble());
+                    //chart1.Series["test2"].Points.AddXY
+                    //                (rdn.NextDouble(), rdn.NextDouble());
+                }
+
+                //chart1.Series["test1"].ChartType = SeriesChartType.FastLine;
+                //chart1.Series["test1"].Color = Color.Red;
+
+                //chart1.Series["test2"].ChartType =
+                //                    SeriesChartType.FastLine;
+                //chart1.Series["test2"].Color = Color.Blue;
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            testRandomNumberGenerator(1000);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            testMultyResult();
+        }
+
+        private void testMultyResult()
+        {
+            string lines = "";
+            double x, y, Area, realResult;
+            int N = Int32.Parse(textBox5.Text);
+            int T = Int32.Parse(textBox7.Text);
+            int Inc = Int32.Parse(textBox6.Text);
+            realResult = (14.0 / 3.0);
+            clearChart(chart3);
+            chart3.ChartAreas[0].AxisY.Maximum = 6;
+            chart3.ChartAreas[0].AxisY.Minimum = 2;
+            for (int j = 0; j < T; j++)
+            {
+                N = N + j * Inc;
+                int K = 0;
+                for (int i = 0; i < N; i++)
+                {
+                    x = GetRandomNumber(1, 4);
+                    y = GetRandomNumber(0, 2);
+                    if ((y <= (Math.Sqrt(x))) && (x <= 4) && (x >= 1) && (y >= 0) && (y <= 2))
+                    {
+                        K++;
+                    }    
+                }
+                Area = ((double)K / (double)N) * 2 * 3;
+                chart3.Series["test1"].Points.AddXY(j, Area);
+                chart3.Series["test2"].Points.AddXY(j, realResult);
+                lines = lines + realResult.ToString() +' ' + Area.ToString() + ' ' + Math.Abs((Area - realResult)).ToString() + "\r\n";
+            }
+            System.IO.StreamWriter file = new System.IO.StreamWriter("c:\\test.txt");
+            file.WriteLine(lines);
+
+            file.Close();
+        }
+
+        private void testMultyResult2()
+        {
+            string lines = "";
+            double x, y, Area, realResult;
+            int N = Int32.Parse(textBox5.Text);
+            int T = Int32.Parse(textBox7.Text);
+            int Inc = Int32.Parse(textBox6.Text);
+            int j;
+            realResult = (14.0 / 3.0);
+            clearChart(chart3);
+            chart3.ChartAreas[0].AxisY.Maximum = 6;
+            chart3.ChartAreas[0].AxisY.Minimum = 2;
+            //for (int j = 0; j < T; j++)
+            {
+                //N = N + j * Inc;
+                int K = 0;
+                j = 0;
+                for (int i = 1; i < N; i++)
+                {
+                    x = GetRandomNumber(1, 4);
+                    y = GetRandomNumber(0, 2);
+                    if ((y <= (Math.Sqrt(x))) && (x <= 4) && (x >= 1) && (y >= 0) && (y <= 2))
+                    {
+                        K++;
+                    }
+
+                    if ( (i % 100) == 0 )
+                    {
+                        j = j + 1;
+                        Area = ((double)K / (double)i) * 2 * 3;
+                        chart3.Series["test1"].Points.AddXY(j, Area);
+                        chart3.Series["test2"].Points.AddXY(j, realResult);
+                    }
+                }
+
+               // lines = lines + realResult.ToString() + ' ' + Area.ToString() + ' ' + Math.Abs((Area - realResult)).ToString() + "\r\n";
+            }
+            System.IO.StreamWriter file = new System.IO.StreamWriter("c:\\test.txt");
+            file.WriteLine(lines);
+
+            file.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            testMultyResult2();
+        }
+    }
+}
